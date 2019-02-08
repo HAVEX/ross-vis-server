a039b8aad01b382e03172b8ef18e32b5088ce23e
Suraj Kesavan <jarus3001@gmail.com>
2019-02-07 20:31:00 -0800

    Progressive Incremental and Incremental PCA

--- pcaHandler.py
+++ pcaHandler.py
@@ -2,6 +2,7 @@ import tornado.web
 import tornado.escape
 import json
 import numpy as np
+import pandas as pd
 from ross_vis.Analytics import Analytics
 
 from webSocketHandler import WebSocketHandler
@@ -14,9 +15,25 @@ class PCAHandler(tornado.web.RequestHandler):
 
     def get(self):
         data = WebSocketHandler.cache.export_dict('KpData')
+
+        # parsing the parameters
+        metrics_in_view_byte_format = self.request.arguments.get('metrics[]')        
+        method_in_byte_format = self.request.arguments.get('method')[0]
+        method = method_in_byte_format.decode('utf8').replace("'", "")
+        print("Computing PC components using {0}".format(method))
+
+        # analysis
         analysis = Analytics(data, index=['Peid', 'Kpid', 'RealTs', 'LastGvt', 'VirtualTs', 'KpGid', 'EventId'])
         analysis.groupby(['Peid', 'Kpid'])
-        result = analysis.pca(2)
+        if method == "PCA":            
+            result = analysis.pca(2)
+        elif method == "prog_inc_PCA":
+            result = analysis.prog_inc_pca(2, 0.1)
+            print(result)
+        elif method == "inc_PCA":
+            result = analysis.inc_pca()
+        elif method == "tsne":
+            result == analysis.a_tsne()
         schema = {k:type(v).__name__ for k,v in data[0].items()}
         self.write({
             'data': result.to_dict('records'),
